3d6cc6f2c3c352a974793232555c4b44c27a4179
newoutsider <90830312+negativexp@users.noreply.github.com>
2023-11-30 08:13:03 +0100

    asdf

--- components/useful.php
+++
@@ -1,6 +0,0 @@
-<?php
-function czechtime($time) {
-    return (new DateTime($time, new DateTimeZone('UTC')))
-        ->setTimezone(new DateTimeZone('Europe/Prague'))
-        ->format('Y/m/d H:i');
-}
